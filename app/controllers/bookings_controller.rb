edf0c36c25e0d5d6d5d7c96d4fc8003de2bcf679
Antonia Lütz <antonialuetz@gmx.de>
2019-11-11 17:24:11 +0100

    controller commented out

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,43 @@
+class BookingsController < ApplicationController
+  # def index
+  #   @bookings = Booking.all
+  # end
+
+  # def show
+  #   @booking = Booking.find(params[:id])
+  # end
+
+  # def new
+  #   @space = Space.find(params[:space_id])
+  #   @booking = Booking.new
+  # end
+
+  # def create
+  #   @booking = Booking.new(params_permit)
+  #   @space = Space.find(params[:space_id])
+  #   @booking.space = @space
+  #   @booking.save
+  #   redirect_to space_path(@space)
+  # end
+
+  # def edit
+  #   @booking = Booking.find(params[:id])
+  # end
+
+  # def update
+  #   @booking = Booking.find(params[:id])
+  #   @booking.update(params_permit)
+  #   redirect_to space_booking_path(:space_id)
+  # end
+
+  # def destroy
+  #   @booking = Booking.find(params[:id])
+  #   @booking.destroy
+  # end
+
+  # private
+
+#   def params_permit
+#     params.require("booking").permit(:startime, :endtime)
+#   end
+end
