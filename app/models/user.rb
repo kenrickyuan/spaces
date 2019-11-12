2094d5dcbf98989f177b52665f111b4e93e16155
Kristina Head <kristina_head@icloud.com>
2019-11-12 10:19:38 +0100

    Added relevant Booking method to the Space controller so we can create a Booking from Space show spage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :spaces, inverse_of: :user, dependent: :destroy
-  has_many :bookings, inverse_of: :user, dependent: :destroy
+  has_many :spaces, inverse_of: 'User', dependent: :destroy
+  has_many :bookings, dependent: :destroy
   validates :name, presence: true
 end
