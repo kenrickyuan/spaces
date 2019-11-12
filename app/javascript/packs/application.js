2a90e34f3cb706183c19974ea131e76f4a8b40f8
Paul Giganti <gigantipaul@gmail.com>
2019-11-12 12:23:03 +0100

    Select2 implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+import $ from 'jquery';
+import 'select2';
+
+const initSelect2 = () => {
+  $('.select2').select2({ width: '100%' }); // (~ document.querySelectorAll)
+};
+
+initSelect2();
