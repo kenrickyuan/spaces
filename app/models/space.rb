3e0ce9904c75d9b1aa949865da0822d62fa75aaf
Kristina Head <kristina_head@icloud.com>
2019-11-12 12:17:11 +0100

    Seed file with 10 instances of user, 10 instances of space and 10 instances of booking for testing

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,6 +2,7 @@ class Space < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :bookings, dependent: :destroy
   validates :owner, presence: true
+  validates :name, presence: true
   validates :description, presence: true
   validates :location, presence: true
   validates :category, presence: true, inclusion: { in: ['house party', 'wedding', 'festival'] }
--- db/migrate/20191111141135_create_spaces.rb
+++ db/migrate/20191111141135_create_spaces.rb
@@ -1,6 +1,7 @@
 class CreateSpaces < ActiveRecord::Migration[5.2]
   def change
     create_table :spaces do |t|
+      t.string :name
       t.string :location
       t.text :description
       t.integer :price_per_hour
