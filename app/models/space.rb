48b5fcac5892656bfe73b026945ab00f333d2dae
Kristina Head <kristina_head@icloud.com>
2019-11-12 10:50:03 +0100

    Added categories to category in Space model

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,7 +4,7 @@ class Space < ApplicationRecord
   validates :owner, presence: true
   validates :description, presence: true
   validates :location, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: ['house party', 'wedding', 'festival'] }
   validates :price_per_hour, presence: true, numericality: true
   validates :max_occupancy, presence: true, numericality: { only_integer: true }
 end
