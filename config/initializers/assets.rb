c61fb770a2d1875b657feef06f276bd2baf66631
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-13 17:48:08 +0100

    heroku bugs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(card-1.jpeg card-2.jpeg card-3.jpeg)
+Rails.application.config.assets.precompile += %w(card-1.png card-2.png card-3.png)
