657a683093ba19487a38eef15a0cbc490f24ec8e
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-13 10:32:31 +0100

    commented trackable out, added new spaces view skeleton

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :omniauthable, :confirmable
+  # :lockable, :timeoutable, :omniauthable, :confirmable, :trackable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable
   has_many :spaces, dependent: :destroy, foreign_key: 'owner_id'
   has_many :bookings, dependent: :destroy
   validates :name, presence: true
--- db/migrate/20191111141123_devise_create_users.rb
+++ db/migrate/20191111141123_devise_create_users.rb
@@ -17,12 +17,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :photo
 
-      ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.inet     :current_sign_in_ip
-      t.inet     :last_sign_in_ip
+      # ## Trackable (TO DO)
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.inet     :current_sign_in_ip
+      # t.inet     :last_sign_in_ip
 
       # # Confirmable
       # t.string   :confirmation_token
