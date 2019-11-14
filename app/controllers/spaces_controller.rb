7ebdd719ad8c156edba0cfd6fe9bc6c40879f730
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-14 23:22:11 +0100

    pushing changes from today

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,7 +2,6 @@ class SpacesController < ApplicationController
   before_action :set_space, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params[:query].present?
       # @spaces = policy_scope(Space.filter(params[:query])) # <= whole pg_search_scope goes in here
       filter_location
       filter_availability if params[:checkin].present? && params[:checkout].present?
@@ -11,10 +10,10 @@ class SpacesController < ApplicationController
       filter_price_per_hour if params[:price_per_hour].present?
       policy_scope(@spaces)
       set_markers
-    else
-      @spaces = policy_scope(Space.all)
-      set_markers
-    end
+    # else
+    #   @spaces = policy_scope(Space.all)
+    #   set_markers
+    # end
   end
 
   def show
@@ -85,7 +84,7 @@ class SpacesController < ApplicationController
     space_ids = @bookings.map(&:space_id)
     @spaces = @spaces.where.not(id: space_ids)
   end
-  
+
   def set_markers
     @spaces_geocoded = @spaces.geocoded
     @markers = @spaces_geocoded.map do |space|
@@ -93,5 +92,6 @@ class SpacesController < ApplicationController
         lat: space.latitude,
         lng: space.longitude
       }
+    end
   end
 end
