8ff48674b60ce3980139080fba94dced0d4b72c6
Kristina Head <kristina_head@icloud.com>
2019-11-14 11:29:12 +0100

    Accidentally started editing seed in master

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@ Booking.destroy_all
 
 puts "Creating 10 users!"
 
+# User for testing
 User.create!(
   name: 'Kristina',
   email: 'kristina_head@icloud.com',
-  password: 'testing123' # Error, I tried using these credentials and I couldn't log into your icloud. Please provide the real password here.
+  password: 'testing123'
   )
 
 10.times do
@@ -23,27 +24,17 @@ end
 
 puts "Creating 10 spaces!"
 
-array_name = ["Fantastic NYC 2 Bed, corner of Central Park!", "Pop House 4min walk to L train 12mins to Manhattan", "LARGE 2 Bedroom Midtown Manhattan Apartment!", "Sunny, Modern room in East Village!", "Exposed Brick Brownstone Apt!", "FULLY Furnished Studio In Manhattan!", "Contemporary, Cozy Retreat", "Private Bedroom In Manhattan", "LARGE SUNNY ROOM, 15MINS TO NYC", "Studio Apartment Minutes Away From Times Square"]
-categories = ['house party', 'wedding', 'festival']
+array_name = ["Fantastic NYC warehouse, corner of Central Park!", "Pop House 4min walk to L train 12mins to Manhattan", "LARGE Midtown Manhattan Apartment!", "Sunny, Modern dinning room in East Village!", "Exposed Brick Brownstone Apt!", "FULLY Furnished Studio In Manhattan!", "Contemporary, Cozy rooftop", "Private garden In Manhattan", "LARGE SUNNY ROOM, 15MINS TO NYC", "Studio Apartment Minutes Away From Times Square"]
+categories = ['house party', 'wedding', 'festival', 'business', 'dinner party', 'drinks', 'birthday', 'coffee/high tea', 'surprise', 'baby shower', 'celebration', 'bridal shower', 'weekend trip', 'bbq']
 
 10.times do |i|
   user = User.all.sample
 
   space = Space.new(
     owner: user,
-    description: "A beautiful NYC apartment. Perfectly situated a block from Central Park, Columbus Circle, Time Warner Center, Whole Foods, Equinox Gym and multiple 5-star dining options. All major subway lines (A,B,C,D,E,N,Q,R,W and 1) are right there!!
-
-    Middle of the burgeoning nightlife of Clinton/Hell’s Kitchen, the culture of Lincoln Center and the charm of the Upper West Side. Full service luxury condo, 24 Hour security, full time doormen. Award winning formal gardens, roof decks for sun and socializing.
-
-    <strong>The space</strong>
-
-    This is an amazing apartment in the absolute heart of Manhattan..
-
-    <strong>Guest access</strong>
-
-    Guests have access the whole apartment",
-    location: Faker::Address.zip_code,
-    category: categories.sample,
+    description: "A beautiful space. Perfectly situated a block from Central Park, Columbus Circle, Time Warner Center, Whole Foods, Equinox Gym and multiple 5-star dining options. All major subway lines (A,B,C,D,E,N,Q,R,W and 1) are right there! Guests have access to all facilities, including wifi!",
+    location: "New York",
+    category: categories.sample(3),
     price_per_hour: rand(1..100),
     max_occupancy: rand(1..100)
     )
