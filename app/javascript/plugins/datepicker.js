af8c7200607c2735a45994b2c9fae2a565806f98
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-14 14:56:23 +0100

    merge complete

--- app/javascript/plugins/datepicker.js
+++ app/javascript/plugins/datepicker.js
@@ -2,11 +2,14 @@ import datetimepicker from "jquery-datetimepicker"
 
 const iniDatePicker = () => {
   $('.picker').datetimepicker({
-    timepicker: false,
+    timepicker: true,
     datepicker: true,
-    format: 'Y-m-d',
-    value: '2019-11-12',
-    weeks: true
+    format: 'Y-m-d H:i',
+    value: '2019-11-12 09:45',
+    hours12: false,
+    step: 30,
+    yearStart: 2019,
+    yearEnd: 2021
   })
 }
 export default iniDatePicker
