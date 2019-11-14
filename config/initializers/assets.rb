8b4983e15934336bfd41d3d624cb15e1bf52087b
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-15 00:35:24 +0100

    link behaviour, nav-link behaviour, minor css fixes, fixed precompile bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w(card-1.png card-2.png card-3.png)
+
+Rails.application.config.assets.precompile += %w( application.css )
