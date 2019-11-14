40b93542570aa120489796c1a64d748edc968a52
Kristina Head <kristina_head@icloud.com>
2019-11-14 11:45:45 +0100

    Edited space description and titles so more generic and applicable to different categories of space. Add more space categories. Changed booking start and end to dynamic dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,23 +24,23 @@ end
 
 puts "Creating 10 spaces!"
 
-array_name = ["Fantastic NYC warehouse, corner of Central Park!", "Pop House 4min walk to L train 12mins to Manhattan", "LARGE Midtown Manhattan Apartment!", "Sunny, Modern dinning room in East Village!", "Exposed Brick Brownstone Apt!", "FULLY Furnished Studio In Manhattan!", "Contemporary, Cozy rooftop", "Private garden In Manhattan", "LARGE SUNNY ROOM, 15MINS TO NYC", "Studio Apartment Minutes Away From Times Square"]
-categories = ['house party', 'wedding', 'festival', 'business', 'dinner party', 'drinks', 'birthday', 'coffee/high tea', 'surprise', 'baby shower', 'celebration', 'bridal shower', 'weekend trip', 'bbq']
+space_name = ["Fantastic NYC warehouse, corner of Central Park!", "Pop House 4min walk to L train 12mins to Manhattan", "LARGE Midtown Manhattan Apartment!", "Sunny, Modern dinning room in East Village!", "Exposed Brick Brownstone Apt!", "FULLY Furnished Studio In Manhattan!", "Contemporary, Cozy rooftop", "Private garden In Manhattan", "LARGE SUNNY ROOM, 15MINS TO NYC", "Studio Apartment Minutes Away From Times Square"]
+categories = ["house party", "wedding", "festival", "business", "dinner party", "drinks", "birthday", "coffee/high tea", "surprise", "baby shower", "celebration", "bridal shower", "weekend trip", "bbq"]
 
 10.times do |i|
   user = User.all.sample
 
   space = Space.new(
     owner: user,
-    description: "A beautiful space. Perfectly situated a block from Central Park, Columbus Circle, Time Warner Center, Whole Foods, Equinox Gym and multiple 5-star dining options. All major subway lines (A,B,C,D,E,N,Q,R,W and 1) are right there! Guests have access to all facilities, including wifi!",
+    description: "A beautiful space. Perfectly situated a block from Central Park, Columbus Circle, Time Warner Center, Whole Foods, Equinox Gym and multiple dining options. All major subway lines (A,B,C,D,E,N,Q,R,W and 1) are right there! Guests have access to all facilities, including wifi",
     location: "New York",
-    category: categories.sample(3),
+    category: categories.sample,
     price_per_hour: rand(1..100),
     max_occupancy: rand(1..100)
     )
 
   position = i - 1
-  space.name = array_name[position]
+  space.name = space_name[position]
   space.save!
 end
 
@@ -53,9 +53,8 @@ puts "Creating 10 bookings!"
   booking = Booking.new(
     user: user,
     space: space,
-    # YYYY-MM-DD HH:MI:SS
-    start_time: '2019-11-12 11:30:00',
-    end_time: '2020-11-12 11:30:00'
+    start_time: Faker::Time.forward(days: rand(1..5),  period: :day, format: :long)
+    end_time: Faker::Time.forward(days: rand(6..10),  period: :day, format: :long)
     )
   booking.save!
 end
