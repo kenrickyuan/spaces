51461f632dd780ad14db10f42400e4aa559ac0b3
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-14 18:19:30 +0100

    query for filter works

--- app/javascript/plugins/datepicker.js
+++ app/javascript/plugins/datepicker.js
@@ -5,7 +5,6 @@ const iniDatePicker = () => {
     timepicker: true,
     datepicker: true,
     format: 'Y-m-d H:i',
-    value: '2019-11-12 09:45',
     hours12: false,
     step: 30,
     yearStart: 2019,
