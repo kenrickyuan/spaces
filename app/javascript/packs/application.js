bd8da76bcf234244eaca93cd343a334c76bea2dc
Paul Giganti <gigantipaul@gmail.com>
2019-11-14 12:58:24 +0100

    datetimepicker added and working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import "bootstrap";
 import $ from 'jquery';
 import 'select2';
+import iniDatePicker from "../plugins/datepicker"
+
+iniDatePicker();
 
 const initSelect2 = () => {
   $('.select2').select2({ width: '100%' }); // (~ document.querySelectorAll)
