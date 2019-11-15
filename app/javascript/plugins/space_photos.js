ca6fbfb8315fb508c5cc59bbc8436e48df2d10d7
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-15 16:10:29 +0100

    lets hope I dont fuck anything up

--- app/javascript/plugins/space_photos.js
+++ app/javascript/plugins/space_photos.js
@@ -4,12 +4,11 @@ const addNewButton = () => {
   console.log(n)
   const html = '<div class="field">' +
     '<div class="form-group file optional space_space_attachments_photo">' +
-      '<label class="file optional" for="space_space_attachments_attributes_' + n + '_photo">Photo</label>' +
       '<input class="form-control-file file optional" type="file" name="space[space_attachments_attributes][' + n + '][photo]" id="space_space_attachments_attributes_' + n + '_photo">' +
     '</div>' +
   '</div>';
   n += 1
-  const formElement = document.getElementById('new_space');
+  const formElement = document.querySelector(".space_space_attachments_photo");
 
   formElement.insertAdjacentHTML('beforeend', html);
 }
