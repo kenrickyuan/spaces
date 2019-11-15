38c2d03268c3e032a47dc9c53b0a8a2a502a488a
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-15 12:24:16 +0100

    able to upload multiple photos in one form, and they are linked to the space model

---
+++ app/javascript/plugins/space_photos.js
@@ -0,0 +1,23 @@
+const addNewButton = () => {
+  let n = 1
+  const html = '<div class="field">' +
+    '<div class="form-group file optional space_space_attachments_photo">' +
+      '<label class="file optional" for="space_space_attachments_attributes_' + n + '_photo">Photo</label>' +
+      '<input class="form-control-file file optional" type="file" name="space[space_attachments_attributes][' + n + '][photo]" id="space_space_attachments_attributes_' + n + '_photo">' +
+    '</div>' +
+  '</div>';
+
+  const formElement = document.getElementById('new_space');
+
+  formElement.insertAdjacentHTML('beforeend', html);
+}
+
+const initMultiplePhotos = () => {
+
+
+  const addPhotoBtn = document.querySelector('.add-photo-btn');
+
+  addPhotoBtn.addEventListener('click', addNewButton);
+}
+
+export default initMultiplePhotos;
