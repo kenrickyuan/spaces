06d0d23978a366640cf9ff800309ad5f95804111
Kristina Head <kristina_head@icloud.com>
2019-11-15 09:23:17 +0100

    Seg fault because of incorrect migration names

--- db/migrate/20191114141517_add_coordinates_to_spaces.rb
+++ db/migrate/20191114141517_add_coordinates_to_spaces.rb
@@ -1,4 +1,4 @@
-class AddCoordinatesToSpace < ActiveRecord::Migration[5.2]
+class AddCoordinatesToSpaces < ActiveRecord::Migration[5.2]
   def change
     add_column :spaces, :latitude, :float
     add_column :spaces, :longitude, :float
